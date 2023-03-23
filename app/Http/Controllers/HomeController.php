06e9cd6d75aceba6a212d01a972ff6558e4abade
Umesh Madushan <71825199+umeshmadushan@users.noreply.github.com>
2023-03-23 19:00:37 +0530

    active student view
    active student view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use domain\Facades\StudentFacade;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.home.index');
+        $response ['students'] = StudentFacade::allActive();
+        return view('pages.home.index')->with($response);
     }
 }
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -8,6 +8,31 @@
                   <h1 class="page-title">Home Page</h1>
             </div>
       </div>
+
+      <div class="col-lg-12 justify-content-center stcardarea">
+
+            @forelse ($students as $student )
+                  
+            <div class="card stcard" style="width: 18rem;">
+                  <img src="{{ config('images.access_path') }}/{{ $student->image }}" class="card-img-top" alt="image">
+                  <div class="card-body">
+
+                    <h5 class="card-title">{{ $student->name }}</h5>
+                    
+                  </div>
+            </div>
+
+            @empty
+
+            <div class="row justify-content-center">
+                  <div class="col-lg-12 text-center">
+                        <h1 class="page-title">No Student Found</h1>
+                  </div>
+                  
+            @endforelse
+
+      </div>
+
 </div>
 
 
@@ -19,6 +44,15 @@
 <style>
       .page-title{
             padding-top: 10vh;
+      }
+
+      .stcardarea{
+            display: flex; 
+            flex-wrap: wrap; 
+      }
+
+      .stcard{
+            margin: 10px;
       }
 </style>
       
